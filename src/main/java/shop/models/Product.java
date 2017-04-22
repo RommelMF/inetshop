a4f2ba06b6bec25b9325464dd6ab854142451523
Rommel <dragondrum@mail.ru>
2017-04-22 09:52:43 +0300

    Created connection to database postgeSQL

---
+++ src/main/java/shop/models/Product.java
@@ -0,0 +1,28 @@
+package shop.models;
+
+import java.util.Map;
+
+/**
+ * Created by Home on 21.04.2017.
+ */
+public interface Product {
+
+     long getId();
+     void setId(long id);
+
+     String getName();
+     void setName(String name);
+
+     String getTitle();
+     void setTitle(String title);
+
+     double getCost();
+     void setCost(double cost);
+
+     long getQuantity();
+     void setQuantity();
+
+     Map<String, String> getMapCharacters();
+     void setMapCharacters(Map<String, String> map);
+
+}
---
+++ src/main/java/shop/repository/DAOProductRepository.java
@@ -0,0 +1,19 @@
+package shop.repository;
+
+import shop.models.Product;
+
+import java.util.List;
+
+/**
+ * Created by Home on 22.04.2017.
+ */
+public interface DAOProductRepository {
+
+    boolean addProduct(Product product);
+
+    boolean deleteProduct(long id);
+
+    Product findProductById(long id);
+
+    List<Product> findProductsList();
+}
